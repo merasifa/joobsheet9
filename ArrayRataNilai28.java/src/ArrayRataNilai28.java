69d9ee419d68f44862246777e728cb84c7bf5a42
merasifa <merasifa@gmail.com>
2023-10-23 14:03:03 +0700

    rata rata

--- ArrayRataNilai28.java/src/ArrayRataNilai28.java
+++ ArrayRataNilai28.java/src/ArrayRataNilai28.java
@@ -3,6 +3,21 @@ import java.util.Scanner;
 
 public class ArrayRataNilai28 {
     public static void main(String[] args) throws Exception {
-        Scanner
+        Scanner scan28 = new Scanner (System.in);
+
+        int[] nilaiMhs= new int [10];
+        double total = 0;
+        double rata2;
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" :");
+            nilaiMhs[i] = scan28.nextInt();
+        }
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            total += nilaiMhs[i];
+        }
+        rata2 = total/nilaiMhs.length;
+        System.out.print("Rata-rata nilai = " + rata2);
     }
 }
